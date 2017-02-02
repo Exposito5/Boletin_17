7d960fc358ee830b7ac757bd6916b8567d413dc1
Exposito5 <aecm.arturo@gmail.com>
2017-02-02 10:53:36 +0100

    Subir

---
+++ src/boletin_17_1/Boletin_17_1.java
@@ -0,0 +1,13 @@
+package boletin_17_1;
+
+public class Boletin_17_1 {
+
+    public static void main(String[] args) {
+
+        Numeros obx = new Numeros();
+        
+        obx.cargarArray();
+        obx.visualizaArrray();
+    }
+
+}
